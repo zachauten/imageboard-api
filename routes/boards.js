d843d8fd70173a4face9c51b03cd244c5ddde7e6
Zach Auten <zachauten@gmail.com>
2018-09-30 15:19:20 -0400

    Added async/await for router methods
    Added error handling, as well as async middleware

--- routes/boards.js
+++ routes/boards.js
@@ -1,40 +1,38 @@
 const router = require('express').Router();
 const db = require('../database');
+const asyncMiddleware = require('../asyncMiddleware');
 
-// get a list of all boards
-router.get('/', async (req, res, next) => {
-    const { rows } = await db.query('select * from boards;');
+router.get('/', asyncMiddleware( async (req, res) => {
+    const { rows } =  await db.query('select * from boards;');
     var json = JSON.stringify(rows);
     res.status(200).send(json);
-});
+}));
 
-// create a new board
-router.post('/', async (req, res) => {
+router.post('/', asyncMiddleware( async (req, res) => {
     const { name } = req.body;
     if (!name) {
-        const error = new Error('Missing board name.');
-        error.httpStatusCode = 400;
-        return next(error);
+        res.status(400);
+        throw new Error('Missing board name.');
     }
     await db.query('insert into boards values($1);', [name]);
     res.status(201).send();
-});
+}));
 
 // Get threads on a board
-router.get('/:board', async (req, res) => {
+router.get('/:board', asyncMiddleware( async (req, res) => {
     const { board } = req.params;
     const { rows } = await db.query('select * from threads where board = $1;', [board]);
     var json = JSON.stringify(rows);
     res.status(200).send(json);
-});
+}));
 
 //TODO: implement pagination
 // get the threads on a page of a board
-router.get('/boards/:board/:page(\d+)', async (req, res) => {
+router.get('/boards/:board/:page(\d+)', asyncMiddleware( async (req, res, next) => {
     const {board} = req.params;
     const { rows } = await db.query('select * from boards where name = $1;', [board]);
     var json = JSON.stringify(rows);
     res.status(200).send(json);
-});
+}));
 
 module.exports = router;
--- routes/threads.js
+++ routes/threads.js
@@ -1,34 +1,32 @@
 const router = require('express').Router();
 const db = require('../database');
+const asyncMiddleware = require('../asyncMiddleware');
 
-// create a thread
-router.post('/', async (req, res) => {
+router.post('/', asyncMiddleware(async (req, res) => {
     const { title, board } = req.body;
     if (!title) {
-        const error = new Error('New thread must have title.');
-        error.httpStatusCode = 400;
-        return next(error);
+        res.status(400);
+        throw new Error('New thread must have title.');
     }
     await db.query('insert into threads(title, board) values($1, $2);', [title, board]);
     res.status(201).send();
-});
+}));
 
 // TODO: change to get all posts from a thread?
-// TODO: add back regex to match endpoint to only integers.
-// get a specific thread
-router.get('/:thread', async (req, res) => {
+// TODO: Use regex on route to only take int
+router.get('/:thread', asyncMiddleware(async (req, res) => {
     const { thread } = req.params;
     const { rows } = await db.query('select * from threads where id = $1;', [thread]);
     var json = JSON.stringify(rows);
     res.status(200).send(json);
-})
+}));
 
-// create a post in a thread
-router.post('/:thread(\d+)', async (req, res) => {
+// TODO: regex (\d+)
+router.post('/:thread', asyncMiddleware(async (req, res) => {
     const { name, commentary } = req.body;
     const thread = req.params.thread;
     await db.query('insert into posts(name, commentary, thread) values($1, $2, $3);', [name, commentary, thread]);
     res.status(201).send();
-});
+}));
 
 module.exports = router;
