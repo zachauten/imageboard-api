260b13984d2cf8dd899319b259b1d3491249beb4
Zach Auten <zachauten@gmail.com>
2018-10-17 19:22:00 -0400

    Added getoneboard test

--- tests/boards.test.js
+++ tests/boards.test.js
@@ -6,22 +6,13 @@ describe('boardController.getAllBoards', () => {
         jest.unmock('../data/boardData');
         var boardData = require.requireActual('../data/boardData');
 
-        var expectedResponse = "foo";
+        var expectedResponse = 'foo';
+        var mockResponse = new MockResponse();
 
-        var mockResponse = {
-            status: (code) => {
-                this.code = code;
-                return this;
-            },
-            send: (body) => {
-                this.body = body;
-                return this;
-            }
-        };
         boardData.getAllBoards = jest.fn(() => expectedResponse);
 
         return await boardController.getAllBoards({}, mockResponse, () => {
-            // boardData.getAllBoards should be called once
+            expect(boardData.getAllBoards).toBeCalledTimes(1);
             expect(mockResponse.code).toBe(200);
             expect(mockResponse.body).toBe('foo');
         });
@@ -32,10 +23,55 @@ describe('boardController.getAllBoards', () => {
     });
 });
 
-descirbe('boardController.getOneBoard', () => {
+describe('boardController.getOneBoard', () => {
 
-    test('should responde with 200 and results from boardData.getOneBoard', () => {
+    test('should respond with 200 and results from boardData.getOneBoard', async () => {
+        jest.unmock('../data/boardData');
+        var boardData = require.requireActual('../data/boardData');
+
+        var expectedResponse = 'foo';
+        var mockResponse = new MockResponse();
+        var mockRequest = new MockRequest({'board': 'foo'}, {})
+
+        boardData.getOneBoard = jest.fn(() => expectedResponse);
+
+        return await boardController.getOneBoard(mockRequest, mockResponse, () => {
+            expect(boardData.getOneBoard).toBeCalledTimes(1);
+            expect(mockResponse.code).toBe(200);
+            expect(mockResponse.body).toBe('foo');
+        });
+    });
+
+    test('should respond with 400 if no board name is passed in request body', async () => {
+
+    });
+
+    test('should respond with 500 if an exception is thrown', async () => {
 
     });
 
 });
+
+describe('boardController.createBoard', () => {
+
+});
+
+describe('boardController.getPage', () => {
+
+});
+
+function MockResponse() {
+    this.status = (code) => {
+        this.code = code;
+        return this;
+    },
+    this.send =  (body) => {
+        this.body = body;
+        return this;
+    }
+};
+
+function MockRequest(params, body) {
+    this.params = params;
+    this.body = body;
+}
