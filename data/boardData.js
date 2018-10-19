a97a1276f65ae885ca84956072ed664430930258
Zach Auten <zachauten@gmail.com>
2018-10-19 19:58:41 -0400

    Removed try / catches from data layer

--- data/boardData.js
+++ data/boardData.js
@@ -1,42 +1,25 @@
 const db = require('./database')
 
 module.exports.getAllBoards = async () => {
-    try {
-        let { rows } = await db.query('select * from boards;')
-        return JSON.stringify(rows)
-    } catch (error) {
-        console.log(error)
-    }
+    let { rows } = await db.query('select * from boards;')
+    return JSON.stringify(rows)
 }
 
 module.exports.getOneBoard = async board => {
-    try {
-        let { rows } = await db.query(
-            'select * from threads where board = $1;',
-            [board]
-        )
-        return JSON.stringify(rows)
-    } catch (error) {
-        console.log(error)
-    }
+    let { rows } = await db.query('select * from threads where board = $1;', [
+        board
+    ])
+    return JSON.stringify(rows)
 }
 
 module.exports.createBoard = async name => {
-    try {
-        await db.query('insert into boards values($1);', [name])
-    } catch (error) {
-        console.log(error)
-    }
+    await db.query('insert into boards values($1);', [name])
 }
 
 module.exports.getPage = async (board, pageSize, offset) => {
-    try {
-        let { rows } = await db.query(
-            'select * from threads where board = $1 order by id limit $2 offset $3',
-            [board, pageSize, offset]
-        )
-        return JSON.stringify(rows)
-    } catch (error) {
-        console.log(error)
-    }
+    let { rows } = await db.query(
+        'select * from threads where board = $1 order by id limit $2 offset $3',
+        [board, pageSize, offset]
+    )
+    return JSON.stringify(rows)
 }
--- data/threadData.js
+++ data/threadData.js
@@ -1,35 +1,22 @@
 const db = require('./database')
 
 module.exports.getOneThread = async thread => {
-    try {
-        let { rows } = await db.query(
-            'select * from posts where thread = $1;',
-            [thread]
-        )
-        return JSON.stringify(rows)
-    } catch (error) {
-        console.log(error)
-    }
+    let { rows } = await db.query('select * from posts where thread = $1;', [
+        thread
+    ])
+    return JSON.stringify(rows)
 }
 
 module.exports.createThread = async (title, board) => {
-    try {
-        await db.query('insert into threads(title, board) values($1, $2);', [
-            title,
-            board
-        ])
-    } catch (error) {
-        console.log(error)
-    }
+    await db.query('insert into threads(title, board) values($1, $2);', [
+        title,
+        board
+    ])
 }
 
 module.exports.createPost = async (name, commentary, thread) => {
-    try {
-        await db.query(
-            'insert into posts(name, commentary, thread) values($1, $2, $3);',
-            [name, commentary, thread]
-        )
-    } catch (error) {
-        console.log(error)
-    }
+    await db.query(
+        'insert into posts(name, commentary, thread) values($1, $2, $3);',
+        [name, commentary, thread]
+    )
 }
