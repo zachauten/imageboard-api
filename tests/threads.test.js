424a060e8f232dc4116053a5ad3e027ea63172db
Zach Auten <zachauten@gmail.com>
2018-10-18 21:16:17 -0400

    Addded threads.test.js boilerplate test cases

---
+++ tests/threads.test.js
@@ -0,0 +1,34 @@
+describe('threadController', () => {
+    const threadController = require('../controllers/threadController');
+    const threadData = require('../data/threadData');
+    const { MockRequest, MockResponse } = require('../tests/mocks');
+    jest.mock('../data/threadData');
+
+    test('getOneThread happy path', async () => {
+
+    });
+
+    test('getOneThread server error', async () => {
+
+    });
+
+    test('createThread happy path', async () => {
+        
+    });
+
+    test('createThread without title', async () => {
+        
+    });
+
+    test('createThread server error', async () => {
+        
+    });
+
+    test('createPost happy path', async () => {
+        
+    });
+
+    test('createPost server error', async () => {
+        
+    });
+});
